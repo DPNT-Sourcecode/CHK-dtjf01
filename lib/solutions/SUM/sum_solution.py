cd311f631443e0c42c6ea45f150fd30bdc2d4f9f
sbx_user1051 <sbx_user1051@169.254.22.97>
2022-06-12 10:03:12 +0000

    Sun Jun 12 10:03:12 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -3,5 +3,15 @@
 # bare with me, im on my laptop keyboard so expect many typos!
 
 def compute(x: int, y: int) -> int:
-    raise NotImplementedError()
+    """Sum two numbers together.
+        0-100
+    """
+    if x < 0 or x > 100:
+        raise ValueError("x must be between 0-100")
+
+    if y < 0 or y > 100:
+        raise ValueError("y must be between 0-100")
+
+    return x + y
+
 

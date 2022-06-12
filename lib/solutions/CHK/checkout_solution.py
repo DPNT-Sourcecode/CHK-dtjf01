31315129abcb9681211544110ec74399d7813944
sbx_user1051 <sbx_user1051@169.254.22.97>
2022-06-12 10:41:56 +0000

    Sun Jun 12 10:41:56 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,9 +15,13 @@ OFFERS = {
 # skus = unicode string
 def checkout(skus: str) -> int:
     # sort the string into groups of SKUs
+    s_skus = sorted(skus.upper())
     # combine offers of SKUs
+
     # calc price
+    total = sum([PRICES.get(s, 0) for s in s_skus])
     # return sum of SKU groups
-    raise NotImplementedError()
+    return total
+
 
 

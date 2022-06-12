bc897811c6ed4e93d4a5b7bfb2fc789a31038cc2
sbx_user1051 <sbx_user1051@169.254.22.97>
2022-06-12 11:35:13 +0000

    Sun Jun 12 11:35:13 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,27 @@ PRICES = {
     "C": 20,
     "D": 15,
     "E": 40,
-    "F": 10
+    "F": 10,
+    "G": 20,
+    "H": 10,
+    "I": 35,
+    "J": 60,
+    "K": 80,
+    "L": 90,
+    "M": 15,
+    "N": 40,
+    "O": 10,
+    "P": 50,
+    "Q": 30,
+    "R": 50,
+    "S": 30,
+    "T": 20,
+    "U": 40,
+    "V": 50,
+    "W": 20,
+    "X": 90,
+    "Y": 10,
+    "Z": 50
 }
 
 OFFERS = {
@@ -47,3 +67,4 @@ def checkout(skus: str) -> int:
     total += sum([PRICES[s] for s in s_skus])
     # return sum of SKU groups
     return total
+

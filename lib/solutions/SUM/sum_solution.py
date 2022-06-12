d62d0f5e50a12d0cf92f69cc3d63fc524a8e81f6
sbx_user1051 <sbx_user1051@169.254.22.97>
2022-06-12 10:00:12 +0000

    Sun Jun 12 10:00:12 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
+
+# bare with me, im on my laptop keyboard so expect many typos!
+
+def compute(x: int, y: int) -> int:
     raise NotImplementedError()
+

3b9f45668db701b485ffe99462c30cace44f2af7
sbx_user1051 <sbx_user1051@169.254.22.97>
2022-06-12 10:35:56 +0000

    Sun Jun 12 10:35:56 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,6 +3,25 @@ import pytest
 from solutions.CHK.checkout_solution import checkout
 
 
+'''
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+'''
 
+@pytest.mark.parmetrize(
+    "skus,expected",
+    [
+        ("A", 50),
+        ("B", 30),
+        ("C", 20),
+        ("D", 15)
+    ]
+)
 def test_checkout(skus: str, expected: int):
     assert checkout(skus) == expected
